591a31487948b110faeb64de7d2519183f003eea
Michael Wigley <39035581+mdwigley@users.noreply.github.com>
2026-02-19 03:11:16 -0700

    ✨ Adding Presenter Control (#12)
    - resolves #11

---
+++ PhotonUI/Controls/Presenter.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PhotonUI.Controls
+{
+    public partial class Presenter(IServiceProvider serviceProvider)
+        : Control(serviceProvider)
+    {
+        [ObservableProperty] private Control? child;
+
+        partial void OnChildChanging(Control? value)
+        {
+            if (child != null)
+                child.Parent = null;
+        }
+        partial void OnChildChanged(Control? value)
+        {
+            if (value != null)
+            {
+                if (value.Parent != null)
+                    throw new InvalidOperationException("Control already has a parent.");
+
+                value.Parent = this;
+
+                this.RequestArrange();
+            }
+        }
+
+        #region Presenter: Framework
+
+        public override bool TunnelControls(Func<Control, bool> traveler, TunnelDirection direction = TunnelDirection.TopDown)
+        {
+            if (direction == TunnelDirection.TopDown)
+            {
+                if (!traveler(this)) return false;
+                if (this.Child != null && !this.Child.TunnelControls(traveler, direction)) return false;
+            }
+            else
+            {
+                if (this.Child != null && !this.Child.TunnelControls(traveler, direction)) return false;
+                if (!traveler(this)) return false;
+            }
+
+            return true;
+        }
+
+        #endregion
+    }
+}
