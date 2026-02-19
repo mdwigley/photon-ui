1038ba14cf8c8dbaaa1ae69b3a56cdfe39646183
Michael Wigley <39035581+mdwigley@users.noreply.github.com>
2026-02-18 22:35:19 -0700

    ✨ Adding entry point and CI/CD (#6)
    - resolves #3
    - resolves #4

---
+++ PhotonUI.Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    static void Main(string[] args)
+    {
+        MainArgsHandler(args);      
+    }
+
+    #region PhotonUI.Demo: Argumentation
+
+    private static void MainArgsHandler(string[] args)
+    {
+        foreach (string arg in args)
+            if (string.IsNullOrEmpty(arg)) { }
+    }
+
+    #endregion
+}
