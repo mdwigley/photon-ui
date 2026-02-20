7d2df1f9b6a36245322cd8306a63fb0885dc4c4e
Michael Wigley <39035581+mdwigley@users.noreply.github.com>
2026-02-19 18:31:51 -0700

    ✨ Adding base Events (#20)
    - resolves #19

---
+++ PhotonUI/Events/FrameworkEventArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace NucleusAF.Photon.Events
+{
+    public class FrameworkEventArgs : EventArgs
+    {
+        public bool Handled { get; set; } = false;
+        public bool Preview { get; set; } = false;
+    }
+}
---
+++ PhotonUI/Events/PlatformEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using SDL3;
+
+namespace NucleusAF.Photon.Events
+{
+    public class PlatformEventArgs(SDL.Event nativeEvent)
+        : FrameworkEventArgs
+    {
+        public SDL.Event NativeEvent { get; } = nativeEvent;
+    }
+}
