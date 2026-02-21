fff88f63ae8c0bc0fadac8ba9a7f6e3041d0bfde
Michael Wigley <39035581+mdwigley@users.noreply.github.com>
2026-02-20 22:38:10 -0700

    ✨ Adding UserControl composite (#42)
    - resolves #41

---
+++ PhotonUI/Controls/Composites/UserControl.cs
@@ -0,0 +1,8 @@
+﻿using PhotonUI.Interfaces.Services;
+
+namespace PhotonUI.Controls.Composites
+{
+    public abstract class UserControl(IServiceProvider serviceProvider, IBindingService bindingService)
+        : Presenter(serviceProvider, bindingService)
+    { }
+}
