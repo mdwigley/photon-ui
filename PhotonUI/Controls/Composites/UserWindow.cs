9746fe714222265d2a8ac1dea5fcd5b2cf2dbf1d
Michael Wigley <39035581+mdwigley@users.noreply.github.com>
2026-02-20 23:44:02 -0700

    ✨ Adding UserWindow control (#46)
    - resolves #45

---
+++ PhotonUI/Controls/Composites/UserWindow.cs
@@ -0,0 +1,8 @@
+﻿using PhotonUI.Interfaces.Services;
+
+namespace PhotonUI.Controls.Composites
+{
+    public partial class UserWindow(IServiceProvider serviceProvider, IBindingService bindingService)
+        : Window(serviceProvider, bindingService)
+    { }
+}
